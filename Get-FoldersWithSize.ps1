128ea1be926b535b3812939209e23bbf4ee56be2
fengshansi <50689156+fengshansi@users.noreply.github.com>
2021-08-08 16:10:49 +0800

    1

---
+++ Get-FoldersWithSize.ps1
@@ -0,0 +1,22 @@
+param ($Path = ".")
+
+$PrettySizeColumn = @{name="Size";expression={
+    $size = $_.Size
+    if ( $size -lt 1KB ) { $sizeOutput = "$("{0:N2}" -f $size) B" }
+    ElseIf ( $size -lt 1MB ) { $sizeOutput = "$("{0:N2}" -f ($size / 1KB)) KB" }
+    ElseIf ( $size -lt 1GB ) { $sizeOutput = "$("{0:N2}" -f ($size / 1MB)) MB" }
+    ElseIf ( $size -lt 1TB ) { $sizeOutput = "$("{0:N2}" -f ($size / 1GB)) GB" }
+    ElseIf ( $size -lt 1PB ) { $sizeOutput = "$("{0:N2}" -f ($size / 1TB)) TB" }
+    ElseIf ( $size -ge 1PB ) { $sizeOutput = "$("{0:N2}" -f ($size / 1PB)) PB" } 
+    $sizeOutput
+}}
+
+Get-ChildItem -Path $Path | Where-Object {$_.PSIsContainer} | ForEach-Object { 
+    $size = ( Get-ChildItem -Path $_.FullName -Recurse -Force | where {!$_.PSIsContainer} | Measure-Object -Sum Length).Sum 
+    $obj = new-object -TypeName psobject -Property @{
+        Path = $_.Name
+        Time = $_.LastWriteTime
+        Size = $size
+    }
+    $obj  
+} | Sort-Object -Property Size -Descending | Select-Object Path, Time, $PrettySizeColumn
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # 脚本收集
  收集常用脚本
+# powershell脚本
+## Get-FoldersWithSize.ps1
+统计子文件夹大小（不统计普通文件）[脚本来源stackoverflow](https://stackoverflow.com/questions/42391237/powershell-folder-size-sort-with-auto-size-conversion)
+ 
+
+
+用法
+```
+#当前文件夹(默认)
+.\Get-FoldersWithSize.ps1
+
+#相对路径
+.\Get-FoldersWithSize.ps1 -Path ".\Downloads"
+
+#绝对路径
+.\Get-FoldersWithSize.ps1 -Path "C:\ProgramData"
+```
